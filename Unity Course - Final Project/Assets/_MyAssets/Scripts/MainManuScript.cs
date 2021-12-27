ece462f03afb2610c1d4387b76faee6c2f578f64
OmriTahar <omritaharnew@gmail.com>
2021-12-27 16:11:45 +0200

    still on menu animation

--- Unity Course - Final Project/Assets/_MyAssets/Scripts/MainManuScript.cs
+++ Unity Course - Final Project/Assets/_MyAssets/Scripts/MainManuScript.cs
@@ -3,16 +3,19 @@ using UnityEngine.SceneManagement;
 
 public class MainManuScript : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
 
-    // Update is called once per frame
-    void Update()
+    [Header("Door Access States")]
+    public GameObject OnPhase;
+    public GameObject StandByPhase;
+    public GameObject OffPhase;
+
+    [Header("References")]
+    public Animator Animator;
+    public Camera ManuCamera;
+
+    public void StartGame()
     {
-        
+
     }
 
     public void QuitGame()
