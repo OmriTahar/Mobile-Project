d8fa7fc91d162720e6c547dc93b79b084e0e9e2c
OmriTahar <omritaharnew@gmail.com>
2021-12-27 16:03:51 +0200

    Trying manu door animation

---
+++ Unity Course - Final Project/Assets/_MyAssets/Scripts/MainManuScript.cs
@@ -0,0 +1,22 @@
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MainManuScript : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void QuitGame()
+    {
+        Application.Quit();
+    }
+}
