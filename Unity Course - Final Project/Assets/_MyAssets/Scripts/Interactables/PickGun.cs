cc840d9de968f7aeb71ab1ea44134e90694111fe
OmriTahar <omritaharnew@gmail.com>
2022-01-01 16:04:28 +0200

    Picked Gun Animation

--- Unity Course - Final Project/Assets/_MyAssets/Scripts/Interactables/PickGun.cs
+++ Unity Course - Final Project/Assets/_MyAssets/Scripts/Interactables/PickGun.cs
@@ -8,15 +8,17 @@ public class PickGun : Interactable
     public GameObject PlayerGun;
     public Image AimButton;
     public GameObject TriggerToActivate;
+    public Animator GunAnimator;
    
     public override void OnInteraction()
     {
         PlayerGun.SetActive(true);
+        GunAnimator.SetTrigger("PickedGun");
+        FindObjectOfType<AudioManager>().PlaySound("Pistol Draw");
+
         AimButton.gameObject.SetActive(true);
         TriggerToActivate.SetActive(true);
 
         Destroy(gameObject);
     }
-
-    
 }
