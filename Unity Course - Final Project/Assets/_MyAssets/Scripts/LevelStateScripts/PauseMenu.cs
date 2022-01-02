20595b194314be3eab13e1ee577cae15e5e7198b
OmriTahar <omritaharnew@gmail.com>
2022-01-02 18:53:53 +0200

    Improved Pause Menu

--- Unity Course - Final Project/Assets/_MyAssets/Scripts/LevelStateScripts/PauseMenu.cs
+++ Unity Course - Final Project/Assets/_MyAssets/Scripts/LevelStateScripts/PauseMenu.cs
@@ -1,16 +1,17 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
+using TMPro;
 
 public class PauseMenu : MonoBehaviour
 {
 
     public FirstPersonController player;
     public Slider cameraSlider;
+    public TextMeshProUGUI CameraSettingsText;
 
     public void ChangeCameraSensitivity()
     {
         player.cameraSensitivity = cameraSlider.value;
+        CameraSettingsText.text = cameraSlider.value.ToString();
     }
 }
